b6c3a57228e1f3f28f9d231ccdebab16ec093acd
xduck7 <empty>
2025-05-19 01:06:58 +0300

    code runner

---
+++ lua/plugins/code_runner.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "CRAG666/code_runner.nvim",
+    dependencies = {
+      "akinsho/toggleterm.nvim",
+      "nvim-lua/plenary.nvim"
+    },
+    config = function()
+      require("code_runner").setup({
+        mode = "toggleterm",
+        filetype = {
+          python = "python3 -u",
+          c = "gcc $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+          cpp = "g++ $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+        },
+        term = {
+          position = "float",
+          size = 15,
+        },
+        runner = {
+          start_insert = true,
+          focus = true,
+        }
+      })
+
+      vim.keymap.set('n', '<leader>rr', ':RunCode<CR>', { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>rf', ':RunFiletype<CR>', { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>rp', ':RunProject<CR>', { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = true })
+    end
+  }
+}
