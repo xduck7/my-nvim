34fbbaf8c870cf56882c2c46fd6e44460bb41bec
xduck7 <empty>
2025-05-19 01:13:22 +0300

    new theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,12 @@
 return {
-	"navarasu/onedark.nvim",	
-	config = function()
-		require('onedark').setup({
-			transparent = true
-		})
-		require('onedark').load()
-	end
+  "rose-pine/neovim",
+  name = "rose-pine",
+  config = function()
+    require("rose-pine").setup({
+      variant = "moon", -- "main", "moon" или "dawn"
+      disable_background = true,
+      disable_float_background = true,
+    })
+    vim.cmd.colorscheme("rose-pine")
+  end
 }
